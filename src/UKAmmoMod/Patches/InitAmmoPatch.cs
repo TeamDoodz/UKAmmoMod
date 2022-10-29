c26eb2c183f57e6c26b71c2bef05809ba4672979
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-28 21:55:26 -0500

    add init ammo patch

---
+++ src/UKAmmoMod/Patches/InitAmmoPatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace UKAmmoMod.Patches {
+	[HarmonyPatch(typeof(NewMovement), nameof(NewMovement.Start))]
+	static class InitAmmoPatch {
+		static void Prefix(NewMovement __instance) {
+			__instance.gameObject.AddComponent<AmmoInventory>();
+		}
+	}
+}
