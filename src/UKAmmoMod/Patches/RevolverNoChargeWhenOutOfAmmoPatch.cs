e991146769103e060ac1ff4885232232465f60fb
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-28 22:37:28 -0500

    revovler uses ammo

---
+++ src/UKAmmoMod/Patches/RevolverNoChargeWhenOutOfAmmoPatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace UKAmmoMod.Patches {
+	[HarmonyPatch(typeof(Revolver), nameof(Revolver.Update))]
+	static class RevolverNoChargeWhenOutOfAmmoPatch {
+		static void Prefix(Revolver __instance) {
+			if(AmmoInventory.Instance.Cells < 6) __instance.pierceCharge = 0f;
+		}
+	}
+}
---
+++ src/UKAmmoMod/Patches/RevolverUseAmmoWhenFiringPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace UKAmmoMod.Patches {
+	[HarmonyPatch(typeof(Revolver), nameof(Revolver.Shoot))]
+	static class RevolverUseAmmoWhenFiringPatch {
+		const int FIRE_NORMAL = 1;
+		const int FIRE_SUPER = 2;
+
+		static bool Prefix(int shotType) {
+			if(shotType == FIRE_SUPER) {
+				if(AmmoInventory.Instance.Cells < 6) return false;
+				AmmoInventory.Instance.Cells -= 6;
+			} else {
+				if(AmmoInventory.Instance.Cells < 1) return false;
+				AmmoInventory.Instance.Cells--;
+			}
+			return true;
+		}
+	}
+}
