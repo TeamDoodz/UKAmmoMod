aded230df4e096ce37d2f9ce5782848dbe38b9b6
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-29 01:21:02 -0500

    rocket launcher uses ammo

---
+++ src/UKAmmoMod/Patches/RocketLauncherUseAmmoWhenFiringPatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace UKAmmoMod.Patches {
+	[HarmonyPatch(typeof(RocketLauncher), nameof(RocketLauncher.Shoot))]
+	static class RocketLauncherUseAmmoWhenFiringPatch {
+		static bool Prefix() {
+			if(AmmoInventory.Instance.Rockets < 1) return false;
+			AmmoInventory.Instance.Rockets--;
+
+			return true;
+		}
+	}
+}
