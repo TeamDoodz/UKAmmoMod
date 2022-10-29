3e37425d70e0fc8eab2aed5aff0fb3c2ae580db6
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-28 22:14:18 -0500

    no ammo system in main menu

--- src/UKAmmoMod/Patches/InitAmmoPatch.cs
+++ src/UKAmmoMod/Patches/InitAmmoPatch.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine.SceneManagement;
 using HarmonyLib;
 
 namespace UKAmmoMod.Patches {
 	[HarmonyPatch(typeof(NewMovement), nameof(NewMovement.Start))]
 	static class InitAmmoPatch {
 		static void Prefix(NewMovement __instance) {
+			if(SceneManager.GetActiveScene().name.StartsWith("Main")) return;
 			__instance.gameObject.AddComponent<AmmoInventory>();
 		}
 	}
