2983eaf79f89b5795d30fa8c1d7fba035d7a311c
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-29 01:15:49 -0500

    nailgun use ammo

---
+++ src/UKAmmoMod/Patches/NailgunNoAmmoPoolWhenOutOfAmmoPatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace UKAmmoMod.Patches {
+	[HarmonyPatch(typeof(Nailgun), nameof(Nailgun.Update))]
+	static class NailgunNoAmmoPoolWhenOutOfAmmoPatch {
+		static void Postfix(Nailgun __instance) {
+			int amount = __instance.altVersion ? 4 : 1;
+
+			if(AmmoInventory.Instance.Nails >= amount) {
+				return;
+			}
+
+			__instance.wc.naiAmmo = 0f;
+			__instance.wc.naiSaws = 0f;
+		}
+	}
+}
