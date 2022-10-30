c3bc92c0d3f50ab69567e76dc19d6acd59e9034e
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-29 21:03:57 -0500

    give ammo on respawn

---
+++ src/UKAmmoMod/Patches/ResetAmmoOnRespawnPatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace UKAmmoMod.Patches {
+	[HarmonyPatch(typeof(NewMovement), nameof(NewMovement.Respawn))]
+	static class ResetAmmoOnRespawnPatch {
+		static void Prefix() {
+			AmmoInventory.Instance.ReplenishAll();
+		}
+	}
+}
