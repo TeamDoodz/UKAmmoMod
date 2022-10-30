dda260daeab00c8d193cbc81d47cc890ccd06dd7
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-29 21:46:32 -0500

    hide ammo ui until activated

---
+++ src/UKAmmoMod/Patches/ShowAmmoUIPatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace UKAmmoMod.Patches {
+	[HarmonyPatch(typeof(PlayerActivator), nameof(PlayerActivator.OnTriggerEnter))]
+	static class ShowAmmoUIPatch {
+		static GameObject? ammoCountersPrefab;
+		static void Prefix(PlayerActivator __instance) {
+			if(__instance.activated) return;
+
+			if(ammoCountersPrefab == null) ammoCountersPrefab = ModAssets.MainBundle.LoadAsset<GameObject>("assets/prefabs/ammocounts.prefab");
+			GameObject.Instantiate(ammoCountersPrefab);
+		}
+	}
+}
