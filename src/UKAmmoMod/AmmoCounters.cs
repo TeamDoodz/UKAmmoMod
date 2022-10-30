e28f13905606f6ac4fd7a7ef27cbea9b978d1ba6
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-29 21:30:30 -0500

    add fx to ammo counters

--- src/UKAmmoMod/AmmoCounters.cs
+++ src/UKAmmoMod/AmmoCounters.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UKAmmoMod.Utilities;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,7 +12,17 @@ namespace UKAmmoMod {
 		[SerializeField] private Text? nailsLabel;
 		[SerializeField] private Text? rocketsLabel;
 
+		private Color cellsOriginalCol;
+		private Color shellsOriginalCol;
+		private Color nailsOriginalCol;
+		private Color rocketsOriginalCol;
+
 		private void Start() {
+			cellsOriginalCol = cellsLabel?.color ?? Color.white;
+			shellsOriginalCol = shellsLabel?.color ?? Color.white;
+			nailsOriginalCol = nailsLabel?.color ?? Color.white;
+			rocketsOriginalCol = rocketsLabel?.color ?? Color.white;
+
 			AmmoInventory.Instance.OnCellsChanged += OnCellsChanged;
 			AmmoInventory.Instance.OnShellsChanged += OnShellsChanged;
 			AmmoInventory.Instance.OnNailsChanged += OnNailsChanged;
@@ -23,24 +34,35 @@ namespace UKAmmoMod {
 			OnRocketsChanged(0, AmmoInventory.Instance.Rockets);
 		}
 
+		private void Update() {
+			cellsLabel.color = ColorUtil.MoveTowards(cellsLabel.color, cellsOriginalCol, Time.deltaTime * 2f);
+			shellsLabel.color = ColorUtil.MoveTowards(shellsLabel.color, shellsOriginalCol, Time.deltaTime * 2f);
+			nailsLabel.color = ColorUtil.MoveTowards(nailsLabel.color, nailsOriginalCol, Time.deltaTime * 2f);
+			rocketsLabel.color = ColorUtil.MoveTowards(rocketsLabel.color, rocketsOriginalCol, Time.deltaTime * 2f);
+		}
+
 		private void OnCellsChanged(int before, int after) {
 			if(cellsLabel == null) return;
 			cellsLabel.text = after.ToString();
+			if(after > before) cellsLabel.color += new Color(0.2f, 0.2f, 0.2f);
 		}
 
 		private void OnShellsChanged(int before, int after) {
 			if(shellsLabel == null) return;
 			shellsLabel.text = after.ToString();
+			if(after > before) shellsLabel.color += new Color(0.1f, 0.1f, 0.1f);
 		}
 
 		private void OnNailsChanged(int before, int after) {
 			if(nailsLabel == null) return;
 			nailsLabel.text = after.ToString();
+			if(after > before) nailsLabel.color += new Color(0.1f, 0.1f, 0.1f);
 		}
 
 		private void OnRocketsChanged(int before, int after) {
 			if(rocketsLabel == null) return;
 			rocketsLabel.text = after.ToString();
+			if(after > before) rocketsLabel.color += new Color(0.1f, 0.1f, 0.1f);
 		}
 	}
 }
---
+++ src/UKAmmoMod/Utilities/ColorUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace UKAmmoMod.Utilities {
+	internal static class ColorUtil {
+		public static Color MoveTowards(Color current, Color target, float maxDelta) {
+			Vector4 outp = Vector4.MoveTowards(current, target, maxDelta);
+			return outp;
+		}
+	}
+}
